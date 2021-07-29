1bdffecbefed1eb35a3fec8250dd357576ce7307
Alex <blackjack1011@icloud.com>
2021-07-29 19:55:47 +0300

    main window

--- #src/js/components/mainWindow.js
+++ #src/js/components/mainWindow.js
@@ -1,108 +1,98 @@
-if(document.getElementById('mainIndexWindow')) {
+if (document.getElementById('mainIndexWindow')) {
 
 // milky way
-gsap.to('.mainWindow__milkyWaySvg', 40, {
-    rotate: 360,
-    ease: 'linear',
-    repeat: -1
-})
+    gsap.to('.mainWindow__milkyWaySvg', 60, {
+        rotate: 360,
+        ease: 'linear',
+        repeat: -1
+    })
 
-gsap.to('.mainWindow__milkyWaySvg path', 8, {
-    rotate: 360,
-    ease: 'linear',
-    repeat: -1
-})
+    gsap.to('.mainWindow__milkyWaySvg path', 8, {
+        rotate: 360,
+        ease: 'linear',
+        repeat: -1
+    })
+
+//circle island
+    gsap.to('.mainWindow__circleArraySvg path', 8, {
+        rotate: 360,
+        ease: 'linear',
+        repeat: -1
+    })
 
 // welcome title
-
-const welcomeTitle = document.querySelector('.mainWindow__title h2')
-const welcomeTitleText = 'welcome'
-welcomeTitle.innerHTML = ''
-let welcomeTitleTextArray = welcomeTitleText.split('').map(letter => `<span class="welcomeTitleLetter">${letter}</span>`)
-welcomeTitleTextArray.forEach(symbol => welcomeTitle.innerHTML += symbol)
+    const welcomeTitle = document.querySelector('.mainWindow__title h2')
+    const welcomeTitleText = 'welcome'
+    welcomeTitle.innerHTML = ''
+    let welcomeTitleTextArray = welcomeTitleText.split('').map(letter => `<span class="welcomeTitleLetter">${letter}</span>`)
+    welcomeTitleTextArray.forEach(symbol => welcomeTitle.innerHTML += symbol)
 
 // creative studio title
-
-let lettersTitleEng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-let lettersTitleRu = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".split("")
-
 // word creative
-let creativeTitle = document.querySelector(".mainWindow__creative")
-let creativeTitleWord = [0,0,0,0,0,0,0,0]
-
-gsap.timeline({ onUpdate: creativeUpdate })
-    .to(creativeTitleWord, 2, { "0": 26 * 3 + 2  }, 0)
-    .to(creativeTitleWord, 2.5, { "1": 26 * 3 + 17 }, 0)
-    .to(creativeTitleWord, 3, { "2": 26 * 3 + 4  }, 0)
-    .to(creativeTitleWord, 3.5, { "3": 26 * 3 + 0 }, 0)
-    .to(creativeTitleWord, 4, { "4": 26 * 3 + 19 }, 0)
-    .to(creativeTitleWord, 4.5, { "5": 26 * 3 + 8 }, 0)
-    .to(creativeTitleWord, 5, { "6": 26 * 3 + 21 }, 0)
-    .to(creativeTitleWord, 5.5, { "7": 26 * 3 + 4 }, 0)
-
-function creativeUpdate() {
-    let html = "";
-    for (let i = 0; i < creativeTitleWord.length; i++) {
-        html += lettersTitleEng[Math.round(creativeTitleWord[i]) % 26]
-    }
-    creativeTitle.innerHTML = html
-}
+    let creativeTitle = document.querySelector(".mainWindow__creative")
+    const creativeTitleText = 'Creative'
+    creativeTitle.innerHTML = ''
+    let creativeTitleTextArray = creativeTitleText.split('').map(letter => `<span class="creativeTitleLetter">${letter}</span>`)
+    creativeTitleTextArray.forEach(symbol => creativeTitle.innerHTML += symbol)
 
 // word studio
-let studioTitle = document.querySelector(".mainWindow__studio")
-let studioTitleWord = [0,0,0,0,0,0]
-
-gsap.timeline({ onUpdate: studioUpdate })
-    .to(studioTitleWord, 2, { "0": 26 * 3 + 18  }, 0)
-    .to(studioTitleWord, 2.5, { "1": 26 * 3 + 19 }, 0)
-    .to(studioTitleWord, 3, { "2": 26 * 3 + 20  }, 0)
-    .to(studioTitleWord, 3.5, { "3": 26 * 3 + 3 }, 0)
-    .to(studioTitleWord, 4, { "4": 26 * 3 + 8 }, 0)
-    .to(studioTitleWord, 5.5, { "5": 26 * 3 + 14 }, 0)
-
-function studioUpdate() {
-    let html = "";
-    for (let i = 0; i < studioTitleWord.length; i++) {
-        html += lettersTitleEng[Math.round(studioTitleWord[i]) % 26]
-    }
-    studioTitle.innerHTML = html
-}
+    let studioTitle = document.querySelector(".mainWindow__studio")
+    const studioTitleText = 'Studio'
+    studioTitle.innerHTML = ''
+    let studioTitleTextArray = studioTitleText.split('').map(letter => `<span class="studioTitleLetter">${letter}</span>`)
+    studioTitleTextArray.forEach(symbol => studioTitle.innerHTML += symbol)
 
 // slogan
-let sloganTitle = document.querySelector(".mainWindow__slogan")
-let sloganTitleWord = [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
-gsap.timeline({ onUpdate: sloganUpdate })
-    .to(sloganTitleWord, 2, { "0": 33 * 3 + 18  }, 0)
-    .to(sloganTitleWord, 2.5, { "1": 33 * 3 + 12 }, 0)
-    .to(sloganTitleWord, 3, { "2": 33 * 3 + 15  }, 0)
-    .to(sloganTitleWord, 3.5, { "3": 33 * 3 + 7 }, 0)
-    .to(sloganTitleWord, 4, { "4": 33 * 3 + 14 }, 0)
-    .to(sloganTitleWord, 4.5, { "5": 33 * 3 + 15 }, 0)
-    .to(sloganTitleWord, 5, { "6": 33 * 3 + 5  }, 0)
-    .to(sloganTitleWord, 5.5, { "7": ' '}, 0)
-    .to(sloganTitleWord, 6, { "8": 33 * 3 + 16  }, 0)
-    .to(sloganTitleWord, 6.5, { "9": 33 * 3 + 17 }, 0)
-    .to(sloganTitleWord, 7, { "10": 33 * 3 + 15 }, 0)
-    .to(sloganTitleWord, 7.5, { "11": 33 * 3 + 18 }, 0)
-    .to(sloganTitleWord, 8, { "12": 33 * 3 + 19 }, 0)
-    .to(sloganTitleWord, 8.5, { "13": 33 * 3 + 15 }, 0)
-
-function sloganUpdate() {
-    let html = "";
-    for (let i = 0; i < sloganTitleWord.length; i++) {
-        html += lettersTitleRu[Math.round(sloganTitleWord[i]) % 33]
-    }
-    sloganTitle.innerHTML = html.replace('А', ' ')
-}
+    let sloganTitle = document.querySelector(".mainWindow__slogan")
+    const sloganTitleText = 'Сложное просто'
+    sloganTitle.innerHTML = ''
+    let sloganTitleTextArray = sloganTitleText.split('').map(letter => `<span class="sloganTitleLetter">${letter}</span>`)
+    sloganTitleTextArray.forEach(symbol => sloganTitle.innerHTML += symbol)
 
 //main animation
+    let indexMainWindowMainAnimation = gsap.timeline({delay: 0})
 
-let indexMainWindowMainAnimation = gsap.timeline()
+    indexMainWindowMainAnimation.to('.welcomeTitleLetter', 3, {
+        opacity: 1,
+        stagger: .2,
+    })
 
-indexMainWindowMainAnimation.to('.welcomeTitleLetter', 3, {
-    opacity: 1,
-    stagger: .4,
-})
+    indexMainWindowMainAnimation.to('.creativeTitleLetter', 2, {
+        opacity: 1,
+        stagger: .2,
+    }, 0)
+
+    indexMainWindowMainAnimation.to('.studioTitleLetter', 2, {
+        opacity: 1,
+        stagger: .3,
+    }, 0)
+
+    indexMainWindowMainAnimation.to('.sloganTitleLetter', 1, {
+        opacity: 1,
+        stagger: .1,
+    }, '-=2')
+
+    indexMainWindowMainAnimation.to('.mainWindow__topLeaves', .5, {
+        transform: 'translateY(0)',
+        stagger: .2
+    }, '-=1')
+
+    indexMainWindowMainAnimation.to('.mainWindow__palmSvg', .5, {
+        transform: 'rotate(-10deg) translateX(30%)'
+    })
+
+    indexMainWindowMainAnimation.to('.mainWindow__palmSvg', .5, {
+        transform: 'rotate(0deg)'
+    })
+
+    indexMainWindowMainAnimation.to('.mainWindow__bottomLeaves', .5, {
+        transform: 'translateY(0)',
+        stagger: .2
+    }, '-=.5')
+
+    indexMainWindowMainAnimation.to('.mainWindow__giraffe', .5, {
+        transform: 'translateX(-50%) translateY(0%)'
+    })
+
 
 }
